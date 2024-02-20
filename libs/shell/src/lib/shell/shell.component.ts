a0993aa29128d95ea0ce46fa36bb3620475460f9
ilkay ivrendi <ilkay.ivrendi_extern@demirtag.de>
2024-02-20 22:33:06 +0300

    shell added

---
+++ libs/shell/src/lib/shell/shell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'aurel-ai-shell',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shell.component.html',
+  styleUrl: './shell.component.scss',
+})
+export class ShellComponent {}
