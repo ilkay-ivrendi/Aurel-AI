ece52dbbe6b55d2b2018c8c28aa827eb72d214b0
ilkay ivrendi <ilkay.ivrendi_extern@demirtag.de>
2024-02-26 02:29:39 +0300

    login component

--- libs/auth/src/lib/login/login.component.ts
+++ libs/auth/src/lib/login/login.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'aurel-ai-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form submitted successfully');
+      console.log('Username:', this.loginForm.value.username);
+      console.log('Password:', this.loginForm.value.password);
+      // Here you can add your authentication logic
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
